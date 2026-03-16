8336bd1a6f4b4cf4593b1d1ddbac5b3b8c6a6b77
mikubob <2386782347@qq.com>
2026-03-16 19:01:02 +0800

    feat(result): 添加序列化支持并优化PageResult类
    - 在PageResult类中添加@Serial注解以支持Java序列化
    - 导入java.io.Serial包用于序列化功能实现
    - 确保PageResult类具备完整的Serializable接口功能
    - 为后续可能的缓存和远程传输场景提供序列化支持

--- DL-common/src/main/java/com/xuan/result/PageResult.java
+++ DL-common/src/main/java/com/xuan/result/PageResult.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 public class PageResult<T> implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     /**

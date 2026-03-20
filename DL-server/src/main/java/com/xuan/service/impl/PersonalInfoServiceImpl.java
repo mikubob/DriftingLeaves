933eef49812a91d88e2079afb072b8699200fb27
mikubob <2386782347@qq.com>
2026-03-20 11:13:56 +0800

    feat(personalInfo): 实现个人信息管理功能
    - 添加MyBatis-Plus查询包装器用于数据库操作
    - 实现管理端获取所有个人信息功能，使用缓存注解优化性能
    - 实现管理端更新个人信息功能，支持数据持久化
    - 实现普通端获取个人信息功能，返回VO对象格式
    - 集成Hutool工具库进行Bean属性复制
    - 添加Spring缓存注解支持，提升查询效率
    - 完善个人信息服务接口的具体实现逻辑

--- DL-server/src/main/java/com/xuan/service/impl/PersonalInfoServiceImpl.java
+++ DL-server/src/main/java/com/xuan/service/impl/PersonalInfoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.xuan.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xuan.dto.PersonalInfoDTO;
 import com.xuan.entity.PersonalInfo;
@@ -8,6 +10,8 @@ import com.xuan.service.IPersonalInfoService;
 import com.xuan.vo.PersonalInfoVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,20 +22,46 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PersonalInfoServiceImpl extends ServiceImpl<PersonalInfoMapper, PersonalInfo> implements IPersonalInfoService {
 
+    /**
+     * 管理端获取所有个人信息
+     * @return 个人信息
+     */
     @Override
+    @Cacheable(value = "personalInfo", key = "'all'")
     public PersonalInfo getAllPersonalInfo() {
-        // TODO: 实现获取个人信息逻辑
-        return null;
+        //1.构建查询条件
+        LambdaQueryWrapper<PersonalInfo> queryWrapper = new LambdaQueryWrapper<>();
+        //2.添加查询条件
+        queryWrapper.eq(PersonalInfo::getId, 1);
+        //3.查询
+        return baseMapper.selectOne(queryWrapper);
+
     }
 
+    /**
+     * 管理端更新个人信息
+     * @param personalInfoDTO 个人信息
+     */
     @Override
+    @CacheEvict(value = "personalInfo", allEntries = true)
     public void updatePersonalInfo(PersonalInfoDTO personalInfoDTO) {
-        // TODO: 实现更新个人信息逻辑
+        PersonalInfo info = BeanUtil.copyProperties(personalInfoDTO, PersonalInfo.class);
+        updateById(info);
     }
 
+    /**
+     * 获取个人信息
+     * @return 个人信息
+     */
     @Override
+    @Cacheable(value = "personalInfo", key = "'vo'")
     public PersonalInfoVO getPersonalInfo() {
-        // TODO: 实现其他端获取个人信息逻辑
-        return null;
+        //1.构建查询条件
+        LambdaQueryWrapper<PersonalInfo> queryWrapper = new LambdaQueryWrapper<>();
+        //2.添加查询条件
+        queryWrapper.eq(PersonalInfo::getId, 1);
+        PersonalInfo info = baseMapper.selectOne(queryWrapper);
+        //3.拷贝属性
+        return BeanUtil.copyProperties(info, PersonalInfoVO.class);
     }
 }
